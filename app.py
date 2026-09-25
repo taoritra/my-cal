bd7245b6dcb33ce055ae3172f06db4f3e203cfc6
taoritra <taoritra@gmail.com>
2026-09-25 23:35:49 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red24\green24\blue24;\red255\green255\blue255;\red13\green62\blue197;
+}
+{\*\expandedcolortbl;;\cssrgb\c12157\c12157\c12157;\cssrgb\c100000\c100000\c100000;\cssrgb\c4314\c34118\c81569;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 2026-09-25 21:27:18.596 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.604 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.604 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.608 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.613 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.617 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.623 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.628 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.632 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.636 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.640 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.642 No runtime found, using MemoryCacheStorageManager\
+2026-09-25 21:27:18.650 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:18.657 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.334 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.335 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.337 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.339 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.340 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.341 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.343 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.344 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.346 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.348 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.350 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.351 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.352 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.353 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.355 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.357 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.358 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.359 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.360 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.361 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.363 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.365 Please replace `use_container_width` with `width`.\
+\
+`use_container_width` will be removed after 2025-12-31.\
+\
+For `use_container_width=True`, use `width='stretch'`. For `use_container_width=False`, use `width='content'`.\
+2026-09-25 21:27:19.370 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.371 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.371 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+2026-09-25 21:27:19.372 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb1 \
+\pard\pardeftab720\qc\partightenfactor0
+
+\f1 \cf4 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf2 \strokec2 \
+\pard\pardeftab720\qc\partightenfactor0
+\cf2 \cb3 \
+}
